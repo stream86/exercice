5a278bfcd72e6bf5126256a5c35ee5f51133cdf4
Alex <stream860@gmail.com>
2021-08-16 20:11:10 +0200

    refactor Context class

--- src/Context/ApplicationContext.php
+++ src/Context/ApplicationContext.php
@@ -2,8 +2,10 @@
 
 namespace App\Context;
 
+use App\Entity\Lesson;
 use App\Entity\Learner;
 use App\Helper\SingletonTrait;
+use App\Contract\NotifyStrategyInterface;
 
 class ApplicationContext
 {
@@ -13,12 +15,29 @@ class ApplicationContext
      * @var Learner
      */
     private Learner $currentUser;
+    private NotifyStrategyInterface $notificationStrategy;
 
     protected function __construct()
     {
 
     }
 
+    /**
+     * @param NotifyStrategyInterface $notificationStrategy
+     */
+    public function setStrategy(NotifyStrategyInterface $notificationStrategy): void
+    {
+        $this->notificationStrategy = $notificationStrategy;
+    }
+
+    /**
+     * @param string $text
+     * @param Lesson $lesson
+     */
+    public function notifyUser(string $text, Lesson $lesson): string
+    {
+        return $this->notificationStrategy->notify($text, $lesson);
+    }
 
     /**
      * @return Learner
